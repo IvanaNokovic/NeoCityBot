67cf4ea9f549f5acc08310b290c22e2eab89529d
IvanaNokovic <nokovic.iva@gmail.com>
2026-03-28 09:04:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const axios = require('axios');
 
 const client = new Client({
   intents: [
@@ -10,7 +9,6 @@ const client = new Client({
 });
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-const KANKA_TOKEN = process.env.KANKA_TOKEN;
 const CAMPAIGN_ID = process.env.CAMPAIGN_ID;
 
 client.on('messageCreate', async (message) => {
@@ -22,13 +20,13 @@ client.on('messageCreate', async (message) => {
     return message.reply('Type a name, e.g. !npc monica');
   }
 
- try {
-  const link = `https://kanka.io/en-US/campaign/${CAMPAIGN_ID}/characters?search=${encodeURIComponent(query)}`;
-  message.channel.send(link);
-} catch (err) {
-  console.error(err);
-  message.reply('Error.');
-}
+  try {
+    const link = `https://kanka.io/en-US/campaign/${CAMPAIGN_ID}/characters?search=${encodeURIComponent(query)}`;
+    message.channel.send(link);
+  } catch (err) {
+    console.error(err);
+    message.reply('Error.');
+  }
 });
 
 client.login(DISCORD_TOKEN);
